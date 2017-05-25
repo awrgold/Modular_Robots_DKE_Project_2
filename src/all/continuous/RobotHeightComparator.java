dc1dc5c716a5349ab9757bb8fe89abbd3971a02c
Infectedd <b.slavchev@student.maastrichtuniversity.nl>
2017-05-25 12:27:17 +0200

    added RobotHeightComparator

--- src/all/continuous/RobotHeightComparator.java
+++ src/all/continuous/RobotHeightComparator.java
@@ -2,9 +2,6 @@ package all.continuous;
 
 import java.util.Comparator;
 
-/**
- * Created by bvsla on 5/23/2017.
- */
 public class RobotHeightComparator implements Comparator<Agent> {
     public int compare(Agent x, Agent y){
         if(x.getLocation().getZ() < y.getLocation().getZ()) return -1;
